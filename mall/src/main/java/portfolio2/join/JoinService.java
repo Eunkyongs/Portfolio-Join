49d38b9d462e99f13f2a6da7b6a71ae71e2af735
Eunkyongs <106173646+Eunkyongs@users.noreply.github.com>
2022-11-06 21:28:32 +0900

    .

--- mall/src/main/java/portfolio2/join/JoinService.java
+++
@@ -1,9 +0,0 @@
-package portfolio2.join;
-
-public interface JoinService {
-	
-	public String insert(JoinDTO joinVO) throws Exception;
-	
-	public JoinDTO selectId(String id) throws Exception;
-		
-}
