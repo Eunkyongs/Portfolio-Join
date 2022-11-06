8f688b65331d8ce217c56c14c5878bc219c25a7c
Eunkyongs <106173646+Eunkyongs@users.noreply.github.com>
2022-11-06 21:28:59 +0900

    .

---
+++ mall/src/main/java/portfolio2/notice/NoticeDTO.java
@@ -0,0 +1,18 @@
+package portfolio2.notice;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NoticeDTO {
+
+	private String no;
+	private String category;
+	private String writer;
+	private String question;
+	private String answer;
+	private String writedate;
+	private String viewdata;
+
+}
